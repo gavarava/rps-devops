6d000c6997db40f369d6c1aeb0011a7edd20b3ef
Gaurav Edekar <edekar.gaurav@gmail.com>
2021-02-23 22:29:13 +0100

    Runnable Gatling Test

--- load-tests/src/main/scala/com/rps/loadtest/gatling/simulations/BasicSimulation.scala
+++ load-tests/src/main/scala/com/rps/loadtest/gatling/simulations/BasicSimulation.scala
@@ -1,13 +1,20 @@
 package com.rps.loadtest.gatling.simulations
 
 import com.typesafe.scalalogging.StrictLogging
-
 import io.gatling.core.Predef._
-import io.gatling.core.feeder.Feeder
-import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
-import io.gatling.http.Predef._
-import io.gatling.http.protocol.HttpProtocolBuilder
+import scala.util.Random
+import io.gatling.http.Predef.http
+import io.gatling.http.protocol.HttpProtocolBuilder.toHttpProtocol
 
 class BasicSimulation extends Simulation with StrictLogging {
+
+  val applicationRootHttp = toHttpProtocol(http
+    .baseUrl("http://localhost"))
+
+  val playerNameFeeder = Iterator.continually(Map("player" -> Random.alphanumeric.take(20).mkString))
+
+  val createPlayerScenario = scenario("Create Player")
+    .exec(http("request_1")
+      .put("/rockpaperscissors/player/" + feed(playerNameFeeder)))
 
 }
--- load-tests/src/main/scala/com/rps/loadtest/gatling/simulations/SimpleSimulation.scala
+++ load-tests/src/main/scala/com/rps/loadtest/gatling/simulations/SimpleSimulation.scala
@@ -1,5 +1,14 @@
 package com.rps.loadtest.gatling.simulations
 
+import io.gatling.core.Predef.{atOnceUsers, nothingFor, openInjectionProfileFactory}
+
+import scala.concurrent.duration.DurationInt
+
 class SimpleSimulation extends BasicSimulation {
-  print("Print Some Text -------- Print Some Text")
+  // Use this setUp to test the scenarios with single users
+  setUp(
+    createPlayerScenario.inject(
+      atOnceUsers(10000),
+    ).protocols(applicationRootHttp)
+  ).maxDuration(1 minutes)
 }
