bfd04bcf30325fbcb00b9201f027a43bbc62dab3
Gaurav Edekar <edekar.gaurav@gmail.com>
2021-02-23 21:50:47 +0100

    Fix Scala Compilation

---
+++ load-tests/src/main/scala/com/rps/loadtest/gatling/simulations/BasicSimulation.scala
@@ -0,0 +1,13 @@
+package com.rps.loadtest.gatling.simulations
+
+import com.typesafe.scalalogging.StrictLogging
+
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.Feeder
+import io.gatling.core.structure.{ChainBuilder, ScenarioBuilder}
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+class BasicSimulation extends Simulation with StrictLogging {
+
+}
--- load-tests/src/main/scala/com/rps/loadtest/gatling/simulations/SimpleSimulation.scala
+++ load-tests/src/main/scala/com/rps/loadtest/gatling/simulations/SimpleSimulation.scala
@@ -1,5 +1,5 @@
 package com.rps.loadtest.gatling.simulations
 
-class SimpleSimulation {
+class SimpleSimulation extends BasicSimulation {
   print("Print Some Text -------- Print Some Text")
 }
